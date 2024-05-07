3c5b5e0f3f98239fecfc8082344e9b29404a7c16
tukint <58389286+tukint@users.noreply.github.com>
2024-05-07 14:07:42 +0300

    MALAM

---
+++ force-app/main/default/lwc/uploadFile/uploadFile.js
@@ -0,0 +1,55 @@
+import { LightningElement, track } from 'lwc';
+import processUploadedContent from '@salesforce/apex/ContactsCreateBatch.processUploadedContent';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class UploadContactsFile extends LightningElement {
+    @track fileContent = '';
+    @track fileName = '';
+    @track isUploadButtonDisabled = true;
+    @track status = ''
+
+    handleFileChange(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                this.fileContent = reader.result;
+                this.fileName = file.name;
+                this.isUploadButtonDisabled = false;
+            };
+            reader.readAsText(file);
+        }
+    }
+
+    handleUpload() {
+        this.status = 'Processing'
+        processUploadedContent({ fileContent: this.fileContent })
+            .then(result => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'File processed successfully: ' + result,
+                        variant: 'success',
+                    }),
+                );
+                this.status = 'Done'
+                this.clearFile();
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error processing file',
+                        message: 'Error: ' + error.body.message,
+                        variant: 'error',
+                    }),
+                );
+            });
+    }
+
+    clearFile() {
+        this.fileContent = '';
+        this.fileName = '';
+        this.isUploadButtonDisabled = true;
+        this.status = ''
+    }
+}
